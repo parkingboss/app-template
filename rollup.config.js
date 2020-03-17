66654430fe98f49e7a2356ea7ba78dba851202b8
Joe Pfohl <jpfohl@parkingboss.com>
2020-03-17 12:38:29 -0400

    fix live reload port dupe / config change crash

--- rollup.config.js
+++ rollup.config.js
@@ -1,66 +1,80 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import svelte from "rollup-plugin-svelte";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
 import copy from "rollup-plugin-copy";
+import getPort from "get-port";
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-	input: 'src/scripts/app.js',
+async function getConfig() {
+  return {
+    input: "src/scripts/app.js",
 
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/app.js'
-	},
+    output: {
+      sourcemap: true,
+      format: "iife",
+      name: "app",
+      file: "public/app.js"
+    },
 
-	plugins: [
-		svelte({
-			dev: !production,
-		}),
+    plugins: [
+      svelte({
+        dev: !production
+      }),
 
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
+      resolve({
+        browser: true,
+        dedupe: importee =>
+          importee === "svelte" || importee.startsWith("svelte/")
+      }),
 
-		commonjs(),
+      commonjs(),
 
-		copy({
-			targets: [
-				{ src: 'src/index.html', dest: 'public' },
-				{ src: 'src/assets/**/*', dest: 'public' },
-			]
-		}),
+      copy({
+        targets: [
+          { src: "src/index.html", dest: "public" },
+          { src: "src/assets/**/*", dest: "public" }
+        ]
+      }),
 
-		!production && serve(),
+      !production && serve(),
 
-		!production && livereload('public'),
+      !production && livereload({
+				name: "public",
+				port: await getPort(),
+				exclusions: ["./rollup.config.js"]
+			}),
 
-		production && terser()
-	],
+      production && terser()
+    ],
 
-	watch: {
-		clearScreen: false
-	}
-};
+    watch: {
+      clearScreen: false
+    }
+  };
 
-function serve() {
-	let started = false;
+  function serve() {
+    let started = false;
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+    return {
+      writeBundle() {
+        if (!started) {
+          started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+          require("child_process").spawn(
+            "npm",
+            ["run", "start", "--", "--dev"],
+            {
+              stdio: ["ignore", "inherit", "inherit"],
+              shell: true
+            }
+          );
+        }
+      }
+    };
+  }
 }
+
+export default getConfig();
